ef217b7fb3b0a8fe9169084dcd13f333a3cb6328
Mamunur Rahman <mamunur@mac-air.home>
2023-09-20 00:21:40 +0100

    alot of JavaScript editing, finally got icon add displayed weather forecast working

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let searchHistory = []
 let searchInput = $("#search-input")
 let searchForm = $("#search-form");
 let searchHistoryContainer = $("#history")
+let todayContainer = $("#today")
 
 function renderSearchHistory() {
     searchHistoryContainer.html("")
@@ -30,6 +31,73 @@ function appendSearchHistory(search) {
     renderSearchHistory()
 }
 
+function renderCurrentWeather(city, weatherData){
+    let date = moment().format("D/M/YYYY")
+    let tempC = weatherData["main"]["temp"];
+    let windKph = weatherData["wind"]["speed"];
+    let humidity = weatherData["main"]["humidity"];
+
+    let iconUrl = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    let iconDescription = weatherData.weather[0].description || weatherData[0].main
+
+    let card = $("<div>")
+    let cardBody = $("<div>")
+    let weatherIcon = $("<img>")
+
+    let heading = $("<h2>")
+    let tempEl = $("<p>")
+    let windEl = $("<p>")
+
+    let humidityEl = $("<p>")
+
+    card.attr("class", "card");
+
+    cardBody.attr("class", "card-body");
+
+    card.append(cardBody);
+
+    heading.attr("class", "h3 card-title")
+    tempEl.attr("class", "card-text")
+    windEl.attr("class", "card-text")
+    humidityEl.attr("class", "card-text");
+
+    heading.text(`${city} (${date})`)
+    weatherIcon.attr("src", iconUrl);
+    weatherIcon.attr("alt", iconDescription);
+
+    heading.append(weatherIcon);
+    tempEl.text(`Temp ${tempC} C`)
+    windEl.text(`Wind ${windKph} KPH`);
+    humidityEl.text(`Humidity ${humidity} %`)
+    cardBody.append(heading, tempEl, windEl, humidityEl);
+
+    todayContainer.html("");
+    todayContainer.append(card);
+
+
+}
+
+function fetchWeather(location){
+    let latitude = location.lat;
+    let longitude = location.lon;
+
+    let city = location.name
+
+    let queryWeatherURL = `${weatherAPIURL}/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${weatherAPIKey}`;
+
+    console.log(queryWeatherURL)
+
+    console.log("Hello")
+    $.ajax({
+        url: queryWeatherURL,
+        method: "GET"
+    }).then(function(response){
+        renderCurrentWeather(city, response.list[0])
+        //renderForecast(data.list);
+    })
+
+}
+
 function fetchCoord(search) {
     let queryURL = `${weatherAPIURL}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherAPIKey}`;
     console.log(queryURL);
@@ -41,23 +109,32 @@ function fetchCoord(search) {
         if (!response[0]) {
             alert("Location not found");
         } else {
-         appendSearchHistory(search)  
+         appendSearchHistory(search)
+         fetchWeather(response[0])  
         }
     });
+}
 
+function initializeHistory(){
+    let storedHistory = localStorage.getItem("search-history");
 
+    if(storedHistory) {
+        searchHistory = JSON.parse(storedHistory);
+    }
+    renderSearchHistory()
 }
 
 function submitSearchForm(event) {
-
     event.preventDefault();
     let search = searchInput.val().trim();
 
     fetchCoord(search);
-
+    searchInput.val("");
 }
 
 
+
+initializeHistory()
 searchForm.on("submit", submitSearchForm);
 
 
